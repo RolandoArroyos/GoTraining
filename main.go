80ebf8b74713a27750839097ff472fdb7fc48400
RolandoArroyos <rolando.arroyos@gmail.com>
2023-08-31 17:51:14 -0500

    Update main.go
    second commit

--- main.go
+++ main.go
@@ -5,5 +5,10 @@ import "fmt"
 func main() {
 
 	fmt.Println("this is working!!!")
+	a, b, c := 24, 911, 90210
+
+	fmt.Printf("a in hex: %#x \n", a)
+	fmt.Printf("b in hex: %#X \n", b)
+	fmt.Printf("c in hex: %#x \n", c)
 
 }
